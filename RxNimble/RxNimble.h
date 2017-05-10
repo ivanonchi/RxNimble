d90a7630a9c65dc9fbd492a8e957c38f49826182
Ivan Luque <ivan.luque@glpgs.com>
2017-05-10 17:18:37 +0900

    Add Carthage support to library.

---
+++ RxNimble/RxNimble.h
@@ -0,0 +1,19 @@
+//
+//  RxNimble.h
+//  RxNimble
+//
+//  Created by Ivan Luque on 2017/05/10.
+//  Copyright © 2017 Ivan Luque. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxNimble.
+FOUNDATION_EXPORT double RxNimbleVersionNumber;
+
+//! Project version string for RxNimble.
+FOUNDATION_EXPORT const unsigned char RxNimbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNimble/PublicHeader.h>
+
+
